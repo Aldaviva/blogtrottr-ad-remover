0c36860d8d7885b260e26c9568500a1f0dc385f8
Ben Hutchison <ben@aldaviva.com>
2014-11-02 01:01:41 -0800

    Updated ad blocking rules for recent Blogtrottr ad changes

--- src/aldaviva.com/blogtrottr-ad-remover/blogtrottr-ad-remover.go
+++ src/aldaviva.com/blogtrottr-ad-remover/blogtrottr-ad-remover.go
@@ -4,10 +4,26 @@ import "code.google.com/p/go.exp/winfsnotify"
 import "fmt"
 import "strings"
 import "io/ioutil"
+import "path/filepath"
 
 const MAIL_DIR = "c:/Programs/Servers/MDaemon/Users/aldaviva.com/ben/Newsfeeds.IMAP"
 
 func main() {
+	removeExistingAds()
+	watchForNewAds()
+}
+
+func removeExistingAds(){
+	msgPaths, err := filepath.Glob(MAIL_DIR + "/*.msg")
+	if err == nil && msgPaths != nil {
+		for _, msgPath := range msgPaths {
+			//fmt.Printf("File exists %s\n", msgPath)
+			removeAdFromFile(msgPath)
+		}
+	}
+}
+
+func watchForNewAds(){
 	watcher, _ := winfsnotify.NewWatcher()
 	watcher.AddWatch(MAIL_DIR, winfsnotify.FS_CREATE)
 
@@ -16,15 +32,15 @@ func main() {
 	for {
 		event := <-watcher.Event
 		filename := event.Name
-		onNewFile(filename)
+		removeAdFromFile(filename)
 	}
 }
 
-func onNewFile(filename string) {
+func removeAdFromFile(filename string) {
 	if strings.HasSuffix(filename, ".msg") {
 		contentsWithAd, _ := ioutil.ReadFile(filename)
 
-		contentsWithoutAd := removeAd(string(contentsWithAd))
+		contentsWithoutAd := removeAdFromBody(string(contentsWithAd))
 
 		ioutil.WriteFile(filename, []byte(contentsWithoutAd), 0777)
 
@@ -32,6 +48,9 @@ func onNewFile(filename string) {
 	}
 }
 
-func removeAd(body string) string {
-	return strings.Replace(body, "style=3D\"border:1=\r\npx solid #555555;", "style=3D\"display:=\r\n none;", -1)
+func removeAdFromBody(body string) string {
+	// return strings.Replace(body, "style=3D\"border:1=\r\npx solid #555555;", "style=3D\"display:=\r\n none;", -1)
+	topAdRemoved := strings.Replace(body, "border=3D\"0\" cellpadding=3D\"0\" cellspacing=3D\"0\"", "style=3D\"display: none;\"", -1)
+	topAndBottomAdRemoved := strings.Replace(topAdRemoved, "cellpadding=3D\"0\" width=3D\"100%\"", "style=3D\"display: none;\"", -1)
+	return topAndBottomAdRemoved
 }
